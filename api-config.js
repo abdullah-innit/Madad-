4f3e03490617c2d16c252f22fdcbc3ecc4e82797
Abdullah <abdullahop31@gmail.com>
2026-07-29 17:01:23 +0500

    Update api-config.js

--- api-config.js
+++ api-config.js
@@ -1,3 +1,1 @@
-// Paste the URL you got from Deploy > New deployment > Web app.
-// It ends in /exec — make sure you copy the whole thing.
 export const API_URL = "https://script.google.com/macros/s/AKfycbwAgd_7YvJXVVb8sr80-jQ9PZkE2w6gTitIUUdnJP-t0YZldyxTjSibIkr1CPLGMTKP/exec";
